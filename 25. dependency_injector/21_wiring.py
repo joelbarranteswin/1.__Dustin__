c73ef2df2c606f9a8771ef0e98f8cd3ae3ceef3a
joelbarranteswin <58517994+joelbarranteswin@users.noreply.github.com>
2021-12-16 18:56:46 -0500

    update

---
+++ 25. dependency_injector/21_wiring.py
@@ -0,0 +1,23 @@
+from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide, inject
+
+
+class Service:
+    ...
+
+
+class Container(containers.DeclarativeContainer):
+
+    service = providers.Factory(Service)
+
+
+@inject
+def main(service: Service = Provide[Container.service]) -> None:
+    ...
+
+
+if __name__ == "__main__":
+    container = Container()
+    container.wire(modules=[__name__])
+
+    main()
